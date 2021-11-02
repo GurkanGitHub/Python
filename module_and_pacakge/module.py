5077b963f03a057988bd1a7509e92970d42930f5
gy <gydatascience@gmail.com>
2021-11-02 21:23:54 +0100

    updates

--- module_and_pacakge/module.py
+++
@@ -1,9 +0,0 @@
-from classes import Planet
-
-
-naboo = Planet("Naboo", 300000, 8, "Naboo System")
-print(f"Name:{naboo.name}")
-print(naboo.spin("a very high speed"))
-
-    #packages: collection of modules.
-    # create a file named space and create an init file
